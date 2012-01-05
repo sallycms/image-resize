a401c312186072be1c9d23a736be44709d9b6aee
Christoph <christoph@webvariants.de>
2012-01-06 00:21:35 +0100

    improved help file

--- help.inc.php
+++ help.inc.php
@@ -1,127 +1,149 @@
 <?php
-/**
- * Image-Resize Addon
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
  *
- * @author office[at]vscope[dot]at Wolfgang Hutteger
- * @author <a href="http://www.vscope.at">www.vscope.at</a>
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
  *
- * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
- * @author zozi@webvariants.de
- *
- *
- * @package sally 0.4
- * @version $Id:
+ * http://www.opensource.org/licenses/mit-license.php
  */
+
+sly_Core::getLayout()->addCSS('
+.iresize-help dl { margin-left: 20px }
+.iresize-help dl.ex dt { float: none; }
+.iresize-help h3 { margin: 15px 0 10px 0; }
+');
+
 ?>
-<h3 class="rex-hl3">Features:</h3>
+<div class="iresize-help">
+	<p>Dieses AddOn erlaubt es, Bilder aus dem Medienpool über speziell präparierte
+	URLs auf eine bestimmte Größe zu skalieren oder Effekte auf sie anzuwenden.</p>
 
-<p class="rex-tx1">Makes resize of images on the fly, with extra cache of resized images so performance loss is extremly small.</p>
+	<h3>Anwendung</h3>
 
-<h3 class="rex-hl3">Usage:</h3>
-<p class="rex-tx1">call an image that way <b>imageresize/100w__imagefile</b>
- to resize the imagefile to a width of 100px</p>
+	<p>Eine Datei namens <strong>test.jpg</strong> muss, um von ImageResize verarbeitet
+	zu werden, über <strong>/imageresize/XXX__test.jpg</strong> aufgerufen werden.
+	<strong>XXX</strong> wird mit den gewünschten Verarbeitungsparametern ersetzt,
+	wie zum Beispiel <strong>100w</strong>, um das Bild auf 100px zu verkleinern:
+	</strong>/imageresize/100w__test.jpg</strong>.<br />
+	Mehrere Aktionen und Filter können über <strong>__</strong> getrennt werden.</p>
 
-<p class="rex-tx1"><b>Notice:</b> if mod-rewrite doesn't work on this server, you can use the old syntax instead:</p>
-<p class="rex-tx1">index.php?rex_resize=100w__imagefile</p>
+	<h3>Aktionen</h3>
 
-<h3 class="rex-hl3">Methods:</h3>
-<p class="rex-tx1">
-w = width       (max width)<br />
-h = height      (max height)<br />
-a = automatic   (max width and max height are the same)<br />
-c = crop        (optional parameter for w, h and a - resize image width or height and crop if nessessary)<br />
-o = offset      (general offset)<br />
-l = left        (offset from left) <br />
-r = right       (offset from right) <br />
-t = top         (offset from top) <br />
-b = bottom      (offset from bottom) <br />
-</p>
+	<dl>
+		<dt>w (width)</dt>
+		<dd>maximale Breite</dd>
 
-<h3 class="rex-hl3">Default-Filters:</h3>
-<p class="rex-tx1">
-blur<br />
-brand<br />
-sepia<br />
-sharpen
-</p>
+		<dt>h (height)</dt>
+		<dd>maximale Höhe</dd>
 
-<h3 class="rex-hl3">Examples:</h3>
-<p class="rex-tx1">
-resize image to a length of 100px and calculate heigt to match ratio<br />
-<b>imageresize/100w__imagefile</b><br />
-or <b>index.php?rex_resize=100w__imagefile</b>
+		<dt>a (automatic)</dt>
+		<dd>maximale Breite &amp; Höhe sind identisch (<strong>100a</strong> equals <strong>100w__100h</strong>)</dd>
 
-<br /><br />
-resize image to a height of 150px and calculate width to match ratio<br />
-<b>imageresize/150h__imagefile</b><br />
-or <b>index.php?rex_resize=150h__imagefile</b>
+		<dt>c (crop)</dt>
+		<dd>Bild auf die angegebenen Maße zuschneiden (Präfix für <strong>w</strong>, <strong>h</strong> und <strong>a</strong>)</dd>
+	</dl>
 
-<br /><br />
-resize image on the longest side to 200px and calculate the other side to match ratio<br />
-<b>imageresize/200a__imagefile</b>
-or <b>index.php?rex_resize=200a__imagefile</b>
+	<p>Statt des gesamten Bildes kann auch nur ein Ausschnitt verarbeitet werden.</p>
 
-<br /><br />
-resize image to a width of 100px and a heigt of 200px<br />
-<b>imageresize/100w__200h__imagefile</b>
-or <b>index.php?rex_resize=100w__200h__imagefile</b>
+	<dl>
+		<dt>o (offset)</dt>
+		<dd>allgemeiner Rahmen</dd>
 
-<br /><br />
-resize image to a heigt of 200px and crop to a width of 100px if nessessary<br />
-<b>imageresize/c100w__200h__imagefile</b>
-or <b>index.php?rex_resize=c100w__200h__imagefile</b>
+		<dt>l (left)</dt>
+		<dd>Abstand von links</dd>
 
-<br /><br />
-resize image to a width of 100px and crop to a height of 200px if nessessary<br />
-<b>imageresize/100w__c200h__imagefile</b>
-or <b>index.php?rex_resize=100w__c200h__imagefile</b>
+		<dt>r (right)</dt>
+		<dd>Abstand von rechts</dd>
 
-<br /><br />
-crop image to a width of 100px and a height of 200px<br />
-<b>imageresize/c100w__c200h__imagefile</b>
-or <b>index.php?rex_resize=c100w__c200h__imagefile</b>
+		<dt>t (top)</dt>
+		<dd>Abstand von oben</dd>
 
-<br /><br />
-resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px<br />
-<b>imageresize/c100w__200h__50o__imagefile</b>
-or <b>index.php?rex_resize=c100w__200h__50o__imagefile</b>
+		<dt>b (bottom)</dt>
+		<dd>Abstand von unten</dd>
+	</dl>
 
-<br /><br />
-resize image to a heigt of 200px and crop to a width of 100px with an offset of -150px<br />
-<b>imageresize/c100w__200h__-150o__imagefile</b>
-or <b>index.php?rex_resize=c100w__200h__-150o__imagefile</b>
+	<h3>Filter</h3>
 
-<br /><br />
-resize image to a heigt of 200px and crop to a width of 100px with an offset of 150px from the right edge<br />
-<b>imageresize/c100w__200h__150r__imagefile</b>
-or <b>index.php?rex_resize=c100w__200h__150r__imagefile</b>
+	<dl>
+		<dt>blur</dt>
+		<dd>Bild verwischen</dd>
 
-<br /><br />
-resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px from the left edge<br />
-<b>imageresize/c100w__200h__50l__imagefile</b>
-or <b>index.php?rex_resize=c100w__200h__50l__imagefile</b>
+		<dt>brand</dt>
+		<dd>Wasserzeichen hinzufügen</dd>
 
-<br /><br />
-resize and crop image to a square of 100x100px<br />
-<b>imageresize/c100a__imagefile</b>
-or <b>index.php?rex_resize=c100a__imagefile</b>
+		<dt>sepia</dt>
+		<dd>Sepia-Filter</dd>
 
-<br /><br />
-add filters: here blur and sepia<br />
-<b>imageresize/200a__fblur__fsepia__imagefile</b>
-or <b>index.php?rex_resize=200a__fblur__fsepia__imagefile</b>
+		<dt>sharpen</dt>
+		<dd>Schärfe erhöhen</dd>
+	</dl>
 
-<br /><br />
-allow upscaling of smaller images:<br />
-<b>imageresize/200a__u__imagefile</b>
+	<h3>Schalter</h3>
 
-<br /><br />
-disable recompression of jpgs:<br />
-<b>imageresize/200a__n__imagefile</b>
+	<dl>
+		<dt>u</dt>
+		<dd>Hochskalieren erlauben</dd>
 
-<br /><br />
-set image type for thumbnail: here 2 for JPEG and 3 for PNG<br />
-<b>imageresize/200a__t2__imagefile</b>
-or <b>imageresize/200a__t3__imagefile</b>
+		<dt>n</dt>
+		<dd>JPEG-Rekompression deaktivieren</dd>
 
-</p>
+		<dt>t2</dt>
+		<dd>JPEG-Datei ausgeben</dd>
+
+		<dt>t3</dt>
+		<dd>PNG-Datei ausgeben</dd>
+	</dl>
+
+	<h3>Beispiele</h3>
+
+	<dl class="ex">
+		<dt>100w__imagefile</dt>
+		<dd>resize image to a length of 100px and calculate heigt to match ratio</dd>
+
+		<dt>150h__imagefile</dt>
+		<dd>resize image to a height of 150px and calculate width to match ratio</dd>
+
+		<dt>200a__imagefile</dt>
+		<dd>resize image on the longest side to 200px and calculate the other side to match ratio</dd>
+
+		<dt>100w__200h__imagefile</dt>
+		<dd>resize image to a width of 100px and a heigt of 200px</dd>
+
+		<dt>c100w__200h__imagefile</dt>
+		<dd>resize image to a heigt of 200px and crop to a width of 100px if nessessary</dd>
+
+		<dt>100w__c200h__imagefile</dt>
+		<dd>resize image to a width of 100px and crop to a height of 200px if nessessary</dd>
+
+		<dt>c100w__c200h__imagefile</dt>
+		<dd>crop image to a width of 100px and a height of 200px</dd>
+
+		<dt>c100w__200h__50o__imagefile</dt>
+		<dd>resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px</dd>
+
+		<dt>c100w__200h__-150o__imagefile</dt>
+		<dd>resize image to a heigt of 200px and crop to a width of 100px with an offset of -150px</dd>
+
+		<dt>c100w__200h__150r__imagefile</dt>
+		<dd>resize image to a heigt of 200px and crop to a width of 100px with an offset of 150px from the right edge</dd>
+
+		<dt>c100w__200h__50l__imagefile</dt>
+		<dd>resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px from the left edge</dd>
+
+		<dt>c100a__imagefile</dt>
+		<dd>resize and crop image to a square of 100x100px</dd>
+
+		<dt>200a__fblur__fsepia__imagefile</dt>
+		<dd>add filters: here blur and sepia</dd>
+
+		<dt>200a__u__imagefile</dt>
+		<dd>allow upscaling of smaller images</dd>
+
+		<dt>200a__n__imagefile</dt>
+		<dd>disable recompression of JPEGs</dd>
+
+		<dt>200a__t2__imagefile</dt>
+		<dd>set image type for thumbnail: here 2 for JPEG and 3 for PNG</dd>
+	</dl>
+</div>
