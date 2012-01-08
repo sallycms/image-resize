ce291ed34595eec7de1c01742b80c71fc7bf54a3
Christoph <christoph@webvariants.de>
2012-01-09 00:48:50 +0100

    fixed duplicate imagedestroy call

---
+++ help.inc.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Image-Resize Addon
+ *
+ * @author office[at]vscope[dot]at Wolfgang Hutteger
+ * @author <a href="http://www.vscope.at">www.vscope.at</a>
+ *
+ * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ * @author zozi@webvariants.de
+ *
+ *
+ * @package sally 0.4
+ * @version $Id:
+ */
+?>
+<h3 class="rex-hl3">Features:</h3>
+
+<p class="rex-tx1">Makes resize of images on the fly, with extra cache of resized images so performance loss is extremly small.</p>
+
+<h3 class="rex-hl3">Usage:</h3>
+<p class="rex-tx1">call an image that way <b>imageresize/100w__imagefile</b>
+ to resize the imagefile to a width of 100px</p>
+
+<p class="rex-tx1"><b>Notice:</b> if mod-rewrite doesn't work on this server, you can use the old syntax instead:</p>
+<p class="rex-tx1">index.php?rex_resize=100w__imagefile</p>
+
+<h3 class="rex-hl3">Methods:</h3>
+<p class="rex-tx1">
+w = width       (max width)<br />
+h = height      (max height)<br />
+a = automatic   (max width and max height are the same)<br />
+c = crop        (optional parameter for w, h and a - resize image width or height and crop if nessessary)<br />
+o = offset      (general offset)<br />
+l = left        (offset from left) <br />
+r = right       (offset from right) <br />
+t = top         (offset from top) <br />
+b = bottom      (offset from bottom) <br />
+</p>
+
+<h3 class="rex-hl3">Default-Filters:</h3>
+<p class="rex-tx1">
+blur<br />
+brand<br />
+sepia<br />
+sharpen
+</p>
+
+<h3 class="rex-hl3">Examples:</h3>
+<p class="rex-tx1">
+resize image to a length of 100px and calculate heigt to match ratio<br />
+<b>imageresize/100w__imagefile</b><br />
+or <b>index.php?rex_resize=100w__imagefile</b>
+
+<br /><br />
+resize image to a height of 150px and calculate width to match ratio<br />
+<b>imageresize/150h__imagefile</b><br />
+or <b>index.php?rex_resize=150h__imagefile</b>
+
+<br /><br />
+resize image on the longest side to 200px and calculate the other side to match ratio<br />
+<b>imageresize/200a__imagefile</b>
+or <b>index.php?rex_resize=200a__imagefile</b>
+
+<br /><br />
+resize image to a width of 100px and a heigt of 200px<br />
+<b>imageresize/100w__200h__imagefile</b>
+or <b>index.php?rex_resize=100w__200h__imagefile</b>
+
+<br /><br />
+resize image to a heigt of 200px and crop to a width of 100px if nessessary<br />
+<b>imageresize/c100w__200h__imagefile</b>
+or <b>index.php?rex_resize=c100w__200h__imagefile</b>
+
+<br /><br />
+resize image to a width of 100px and crop to a height of 200px if nessessary<br />
+<b>imageresize/100w__c200h__imagefile</b>
+or <b>index.php?rex_resize=100w__c200h__imagefile</b>
+
+<br /><br />
+crop image to a width of 100px and a height of 200px<br />
+<b>imageresize/c100w__c200h__imagefile</b>
+or <b>index.php?rex_resize=c100w__c200h__imagefile</b>
+
+<br /><br />
+resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px<br />
+<b>imageresize/c100w__200h__50o__imagefile</b>
+or <b>index.php?rex_resize=c100w__200h__50o__imagefile</b>
+
+<br /><br />
+resize image to a heigt of 200px and crop to a width of 100px with an offset of -150px<br />
+<b>imageresize/c100w__200h__-150o__imagefile</b>
+or <b>index.php?rex_resize=c100w__200h__-150o__imagefile</b>
+
+<br /><br />
+resize image to a heigt of 200px and crop to a width of 100px with an offset of 150px from the right edge<br />
+<b>imageresize/c100w__200h__150r__imagefile</b>
+or <b>index.php?rex_resize=c100w__200h__150r__imagefile</b>
+
+<br /><br />
+resize image to a heigt of 200px and crop to a width of 100px with an offset of 50px from the left edge<br />
+<b>imageresize/c100w__200h__50l__imagefile</b>
+or <b>index.php?rex_resize=c100w__200h__50l__imagefile</b>
+
+<br /><br />
+resize and crop image to a square of 100x100px<br />
+<b>imageresize/c100a__imagefile</b>
+or <b>index.php?rex_resize=c100a__imagefile</b>
+
+<br /><br />
+add filters: here blur and sepia<br />
+<b>imageresize/200a__fblur__fsepia__imagefile</b>
+or <b>index.php?rex_resize=200a__fblur__fsepia__imagefile</b>
+
+<br /><br />
+allow upscaling of smaller images:<br />
+<b>imageresize/200a__u__imagefile</b>
+
+<br /><br />
+disable recompression of jpgs:<br />
+<b>imageresize/200a__n__imagefile</b>
+
+<br /><br />
+set image type for thumbnail: here 2 for JPEG and 3 for PNG<br />
+<b>imageresize/200a__t2__imagefile</b>
+or <b>imageresize/200a__t3__imagefile</b>
+
+</p>
