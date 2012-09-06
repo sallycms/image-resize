d67caf2f4fa1de62f44810315c510b55adb038c1
Zozi <zozi@webvariants.de>
2012-09-06 22:27:34 +0200

    apply a fix for the max_cachefiles problem

--- lib/A2/Thumbnail.php
+++ lib/A2/Thumbnail.php
@@ -93,13 +93,9 @@ class A2_Thumbnail {
 		$this->width      = $this->origWidth;
 		$this->height     = $this->origHeight;
 
-		$service = sly_Service_Factory::getAddOnService();
-		$is06    = sly_Core::getVersion('X.Y') === '0.6';
-		$name    = $is06 ? 'image_resize' : 'sallycms/image-resize';
-
-		$this->thumbQuality     = (int) $service->getProperty($name, 'jpg_quality', $this->thumbQuality);
-		$this->upscalingAllowed = (bool) $service->getProperty($name, 'upscaling_allowed', $this->upscalingAllowed);
-		$this->compressJPG      = (bool) $service->getProperty($name, 'recompress', $this->compressJPG);
+		$this->thumbQuality     = (int)  A2_Util::getProperty('jpg_quality', $this->thumbQuality);
+		$this->upscalingAllowed = (bool) A2_Util::getProperty('upscaling_allowed', $this->upscalingAllowed);
+		$this->compressJPG      = (bool) A2_Util::getProperty('recompress', $this->compressJPG);
 	}
 
 	/**
