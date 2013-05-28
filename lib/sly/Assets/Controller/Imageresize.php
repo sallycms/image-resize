35699ff9b09ab180e723b63a01fedb65beabb6e5
Christoph <christoph@webvariants.de>
2013-05-28 23:14:15 +0200

    more the process listener into a separate method, since we already have a class for that

--- lib/sly/Assets/Controller/Imageresize.php
+++ lib/sly/Assets/Controller/Imageresize.php
@@ -29,45 +29,18 @@ class Imageresize extends Base {
 
 		// add our process listener (do it here to remember the requested filename)
 
-		$filename    = Filename::parse($file);
-		$container   = $this->getContainer();
-		$dispatcher  = $container->getDispatcher();
-		$service     = $container->get('sly-imageresize-service');
-		$jsonService = $container->get('sly-service-json');
-		$controlFile = $service->getControlFile($filename->getFilename());
+		$filename = Filename::parse($file);
 
 		if ($filename->hasModifications()) {
-			$dispatcher->addListener(AssetService::EVENT_PROCESS_ASSET, function($inputFile) use ($filename, $service, $jsonService, $controlFile) {
-				$controlData = file_exists($controlFile) ? $jsonService->load($controlFile, false, true) : array();
+			$container  = $this->getContainer();
+			$dispatcher = $container->getDispatcher();
+			$listeners  = $container->get('sly-imageresize-listeners');
 
-				// prepare the thumbnail
-
-				$config    = $service->getConfig(null);
-				$thumbnail = $filename->getThumbnail($config, $inputFile);
-				$realName  = $filename->getFilename();
-
-				// remove old cache files
-
-				if (count($controlData) >= $config['max_cachefiles'] && !in_array($realName, $controlData)) {
-					// TODO
-				}
-
-				// process the image
-
-				$tmpDir  = $service->getCacheDir();
-				$tmpFile = $tmpDir.'/tmp_'.sha1($filename->getVirtualFilename()).'.'.$filename->getExtension();
-
-				$thumb->generateImage($tmpFile);
-
-				$controlData[] = $realName;
-				$jsonService->dump($controlFile, $controlData);
-
-				return $tmpFile;
-			});
+			$dispatcher->addListener(AssetService::EVENT_PROCESS_ASSET, array($listeners, 'processAsset'), compact('filename'));
 		}
 
 		// send the requested file, we will perform the manipulations later in the
-		// process events from the asset service
+		// process event from the asset service
 
 		return $this->sendFile('sly://media/'.$filename->getFilename(), true, true, true);
 	}
